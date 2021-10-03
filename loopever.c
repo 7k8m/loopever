c453387b0ea88f95e4b7b968d5acd496eb6936e7
Tomohito Nakayama <tmht@7k8m.me>
2021-10-03 17:01:42 +0900

    Initial commit

---
+++ loopever.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(int argc, char **argv){
+	
+	if(argc < 2){
+		fprintf(stderr,"Specify sleep sec\n"); 
+		return 1;
+	}
+	
+	char *endPtr = NULL;
+	errno = 0;
+	long sleepsec = strtol(*(argv + 1),&endPtr,10);
+	if(*endPtr != '\0' || errno != 0){
+		fprintf(stderr,"Failed to strol sleepsec.\n");
+		return 1;
+	}	
+	
+	while(1){
+		sleep(sleepsec);
+	}
+	return 0;
+}	
