39173b5d5473d198bf898d1350d0cf2c85f165a9
Tomohito Nakayama <tmht@7k8m.me>
2021-10-03 20:13:00 +0900

    Formatted whole code

--- loopever.c
+++ loopever.c
@@ -3,23 +3,27 @@
 #include <unistd.h>
 #include <errno.h>
 
-int main(int argc, char **argv){
-	
-	if(argc < 2){
-		fprintf(stderr,"Specify sleepsec\n"); 
+int main(int argc, char **argv)
+{
+
+	if (argc < 2)
+	{
+		fprintf(stderr, "Specify sleepsec\n");
 		return 1;
 	}
-	
+
 	char *endPtr = NULL;
 	errno = 0;
-	long sleepsec = strtol(*(argv + 1),&endPtr,10);
-	if(*endPtr != '\0' || errno != 0){
-		fprintf(stderr,"Failed to strol sleepsec.\n");
+	long sleepsec = strtol(*(argv + 1), &endPtr, 10);
+	if (*endPtr != '\0' || errno != 0)
+	{
+		fprintf(stderr, "Failed to strol sleepsec.\n");
 		return 1;
-	}	
-	
-	while(1){
+	}
+
+	while (1)
+	{
 		sleep(sleepsec);
 	}
 	return 0;
-}	
+}
